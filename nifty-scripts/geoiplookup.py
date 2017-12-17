e1267cc95489b9076022af5909a4291053afb088
nikkonlp <nikko.pedersen@gmail.com>
2017-12-17 13:25:49 -0700

    Adding script to lookup geolocation of IP

---
+++ nifty-scripts/geoiplookup.py
@@ -0,0 +1,25 @@
+#!/bin/python
+# Author: Nikko Pedersen
+# Revised: 12/17/2017
+# Purpose: Prints the Geo Location of an IP passed
+# Execute: python geoip.py <ip>
+# Set Bash alias: alias geoip='python /home/git/admin-scripts/nifty-scripts/geoip.py'
+# ---------------------------------------
+### Imports
+from sys import argv
+from geoip import geolite2
+
+### Take first argument passed as IP
+ip = argv[1]
+
+### Lookup geolocation of IP
+geoip = geolite2.lookup(ip)
+
+### If the IP has an associated state - define state
+if str(geoip.subdivisions)[12:14].isalpha():
+	state = ", " + str(geoip.subdivisions)[12:14]
+else:
+	state = ""
+
+### Print details
+print ip + ": " + str(geoip.country) + state
--- nifty-scripts/port-service-lookup.py
+++ nifty-scripts/port-service-lookup.py
@@ -2,7 +2,7 @@
 # Author: Nikko Pedersen
 # Revised: 12/17/2017
 # Purpose: Looks up the service correlated with a port
-# Execute: python port-service-lookup.py
+# Execute: python port-service-lookup.py <port>
 # Set Bash alias: alias ports='python /home/git/admin-scripts/nifty-scripts/port-service-lookup.py'
 # ---------------------------------------
 ### Imports
